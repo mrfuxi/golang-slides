0d8210825ed24e921fa5a50bced2a8a3caa75088
Karol Duleba <mr.fuxi@gmail.com>
2016-01-13 23:12:38 +0000

    Interface slide split into two

--- intro/interface.go
+++ intro/interface.go
@@ -5,18 +5,21 @@ import (
 	"math"
 )
 
+// START OMIT
 type ShapeArea interface {
 	Area() float64
 }
 
 type Square struct{ Side float64 }
 
-func (s Square) Area() float64 { return s.Side * s.Side }
+func (s Square) Area() float64  { return s.Side * s.Side }
+func (s Square) String() string { return fmt.Sprintf("<Sqare %v>", s.Side) }
 
 type Circle struct{ Radius float64 }
 
 func (c Circle) Area() float64 { return math.Pi * c.Radius * c.Radius }
 
+// MID OMIT
 func SumAreas(shape ...ShapeArea) (sum float64) {
 	for _, f := range shape {
 		sum += f.Area()
@@ -31,3 +34,5 @@ func main() {
 	sum := SumAreas(sqare1, sqare2, circle)
 	fmt.Printf("%v + %v + %v = %v\n", sqare1, sqare2, circle, sum)
 }
+
+// END OMIT
