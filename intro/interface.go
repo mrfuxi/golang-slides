04e6d8c9a5430ad6e375d01fbc9647ad661e65b5
Karol Duleba <mr.fuxi@gmail.com>
2016-01-13 23:01:55 +0000

    Start of interface slide

---
+++ intro/interface.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type ShapeArea interface {
+	Area() float64
+}
+
+type Square struct{ Side float64 }
+
+func (s Square) Area() float64 { return s.Side * s.Side }
+
+type Circle struct{ Radius float64 }
+
+func (c Circle) Area() float64 { return math.Pi * c.Radius * c.Radius }
+
+func SumAreas(shape ...ShapeArea) (sum float64) {
+	for _, f := range shape {
+		sum += f.Area()
+	}
+	return
+}
+
+func main() {
+	sqare1 := Square{Side: 1}
+	sqare2 := Square{Side: 2}
+	circle := Circle{Radius: 3}
+	sum := SumAreas(sqare1, sqare2, circle)
+	fmt.Printf("%v + %v + %v = %v\n", sqare1, sqare2, circle, sum)
+}
